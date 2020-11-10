6cdd700c64bb4bc42ca41fd12e6924bdc4e9f493
gazdagergo <info@gazdagergo.com>
2020-11-10 12:20:23 +0100

    add border

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import "./styles.css";
 import styled from "styled-components";
 import WebcamCapture from "./WebcamCapture";
-
-const AppStyled = styled.div`
-  background: black;
-`;
+import GlobalStyle from "./GlobalStyle";
 
 export default function App() {
   return (
-    <AppStyled>
+    <div>
+      <GlobalStyle />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <WebcamCapture />
-    </AppStyled>
+    </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,7 +1,25 @@
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
+import styled from "styled-components";
 
-const WebcamCapture = () => {
+const CamWrap = styled.div`
+  position: relative;
+  border: 2px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardBorder = styled.div`
+  position: absolute;
+  border: 2px solid #ffffffcc;
+  box-shadow: 0 0 34px 0 black;
+  border-radius: 3vw;
+  width: 80vw;
+  height: calc(80vw * 0.63);
+`;
+
+const WebcamCapture = ({ className }) => {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
 
@@ -12,7 +30,16 @@ const WebcamCapture = () => {
 
   return (
     <>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      <CamWrap>
+        <Webcam
+          style={{ width: "100vw" }}
+          audio={false}
+          mirrored
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+        />
+        <CardBorder />
+      </CamWrap>
       <button onClick={capture}>Capture photo</button>
       {imgSrc && <img alt="pic" src={imgSrc} />}
     </>
